5dbfaaa25e2bb8fccdd33427b780cc455f4ddd3b
madsystem <madsystem@hotmail.com>
2016-01-02 02:13:07 +0100

    setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,16 @@
 # ENV_DIR needs to be set
 cd ~
 
+#dot files
 DOT_FILES=$ENV_DIR/linux-env/dot-files
-echo Installing dotfiles from $DOT_FILES 
 rm .xinitrc
 rm .bashrc
+rm .xbindkeysrc
 ln -s $DOT_FILES/.xinitrc .
 ln -s $DOT_FILES/.bashrc .
+ln -s $DOT_FILES/.xbindkeysrc .
 
+#script dir
 SCRIPT_DIR=$ENV_DIR/linux-env/script
-echo Installing script dir from $SCRIPT_DIR
 ln -s $SCRIPT_DIR $ENV_DIR
 
