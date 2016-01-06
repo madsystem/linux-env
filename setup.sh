d7ec4a14ac51d07b62d819118df8c2759744b800
madsystem <you@example.com>
2016-01-06 01:33:25 +0100

    cleanup, add mate settings daemon to xinit rc.

--- setup.sh
+++ setup.sh
@@ -6,16 +6,12 @@ cd ~
 
 #dot files
 DOT_FILES=$ENV_DIR/linux-env/dot-files
-rm .xinitrc
-rm .bashrc
-rm .xbindkeysrc
-rm .zshrc
+rm -f .xinitrc
+rm -f .bashrc
+rm -f .xbindkeysrc
+rm -f .zshrc
 ln -s $DOT_FILES/xinitrc .xinitrc
 ln -s $DOT_FILES/bashrc .bashrc 
 ln -s $DOT_FILES/xbindkeysrc .xbindkeysrc 
 ln -s $DOT_FILES/zshrc .zshrc 
-
-#script dir
-SCRIPT_DIR=$ENV_DIR/linux-env/script
-ln -s $SCRIPT_DIR $ENV_DIR
 
