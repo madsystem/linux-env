932aa5a00d5dfa80a3b108b86b2f72a7bdff1905
madsystem <you@example.com>
2016-07-02 03:44:25 +0200

    i3 added / dwm also changed (unknown)

--- setup.sh
+++ setup.sh
@@ -10,8 +10,11 @@ rm -f .xinitrc
 rm -f .bashrc
 rm -f .xbindkeysrc
 rm -f .zshrc
+rm -f ~/.config/i3/config
 ln -s $DOT_FILES/xinitrc .xinitrc
 ln -s $DOT_FILES/bashrc .bashrc 
 ln -s $DOT_FILES/xbindkeysrc .xbindkeysrc 
 ln -s $DOT_FILES/zshrc .zshrc 
+ln -s $DOT_FILES/zshrc .zshrc 
+ln -s $DOT_FILES/i3 ~/.config/i3/config
 
