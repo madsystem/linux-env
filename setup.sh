56c772d53e6b4ff9066267f520d87a19651882d6
madsystem <you@example.com>
2016-07-10 23:25:08 +0200

    i3 configuration

--- setup.sh
+++ setup.sh
@@ -5,16 +5,26 @@
 cd ~
 
 #dot files
-DOT_FILES=$ENV_DIR/linux-env/dot-files
+LINUX_ENV=$ENV_DIR/linux-env/
+DOT_FILES=$LINUX_ENV/dot-files
 rm -f .xinitrc
 rm -f .bashrc
 rm -f .xbindkeysrc
 rm -f .zshrc
 rm -f ~/.config/i3/config
+rm -f ~/.config/i3/i3status.conf
+rm -f ~/.config/rofi_colors
 ln -s $DOT_FILES/xinitrc .xinitrc
 ln -s $DOT_FILES/bashrc .bashrc 
 ln -s $DOT_FILES/xbindkeysrc .xbindkeysrc 
 ln -s $DOT_FILES/zshrc .zshrc 
-ln -s $DOT_FILES/zshrc .zshrc 
 ln -s $DOT_FILES/i3 ~/.config/i3/config
+ln -s $DOT_FILES/i3status ~/.config/i3/i3status.conf
+ln -s $DOT_FILES/rofi_colors ~/.config/rofi_colors
 
+#fonts
+cp -a $LINUX_ENV/fonts/. ~/.fonts/
+
+#wallpaper
+rm -f ~/.config/wallpaper*
+ln -s $LINUX_ENV/wallpaper.png ~/.config/wallpaper.png
